ff8f3ba73814e2db1f244d28383d65bb5d5d8412
varun2005cmd <136258677+varun2005cmd@users.noreply.github.com>
2024-10-22 13:49:22 +0530

    Create 20a.cpp

---
+++ 20a.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void s_sort(int *a, int elements) {
+    int n = 0;
+    int *b;
+
+    while (n < elements) {
+        b = a + 1;
+        for (int i = 0; i < (elements - 1) - n; i++) {
+            if (*a > *b) {
+                swap(a, b);
+            }
+            b++;
+        }
+        n++;
+        a++;
+    }
+}
+
+int main() {
+    int no_el;
+    cout << "How many elements in your array?" << endl;
+    cin >> no_el;
+    int arr[no_el];
+
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < no_el; i++) {
+        cin >> arr[i];
+    }
+
+    s_sort(arr, no_el);
+
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < no_el; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << end
